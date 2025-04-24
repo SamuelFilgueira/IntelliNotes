3b2cd63c28c9fb1cf6cd3423f8626d34f5ef673f
SamuelFilgueira <leumas685@gmail.com>
2025-04-24 16:41:03 -0300

    Criação das entidades finalizada.

---
+++ src/backend/IntelliNotes.API/Entities/Note.cs
@@ -0,0 +1,23 @@
+﻿namespace IntelliNotes.API.Entities
+{
+    public class Note
+    {
+        public Note(string title, string content)
+        {
+            Title = title;
+            Content = content;
+
+            CreatedAt = DateTime.Now;
+        }
+
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public string? Summary { get; private set; }
+        public string? Category { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? UpdatedAt { get; private set; }
+        public User User { get; set; }
+    }
+}
---
+++ src/backend/IntelliNotes.API/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿namespace IntelliNotes.API.Entities
+{
+    public class User
+    {
+        public User(string userName, string email)
+        {
+            UserName = userName;
+            Email = email;
+            Id = Guid.NewGuid();
+            Role = "User";
+
+            CreatedAt = DateTime.Now;
+            Notes = new List<Note>();
+        }
+
+        public Guid Id { get; private set; }
+        public string UserName { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; } = string.Empty;
+        public string Role { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public ICollection<Note> Notes { get; private set; }
+    }
+}
