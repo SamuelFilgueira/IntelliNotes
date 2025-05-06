c269d7de7a3ddbefe768c7c3baa437c63af3e1f7
Samuel Filgueira <leumas685@gmail.com>
2025-05-06 05:37:19 -0300

    Criando e configurando os mapeamentos

--- src/backend/IntelliNotes.Infrastructure/DependecyInjection/DependencyInjection.cs
+++ src/backend/IntelliNotes.Infrastructure/DependecyInjection/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using IntelliNotes.Infrastructure.Context;
+﻿using IntelliNotes.Application.Mappings;
+using IntelliNotes.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,11 +11,15 @@ namespace IntelliNotes.Infrastructure.DependecyInjection
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             Console.WriteLine("Connection string: " + configuration.GetConnectionString("DefaultConnection"));
-
             services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             });
+
+            //Configurando mapping service
+            services.AddAutoMapper(typeof(UserProfile).Assembly);
+            services.AddAutoMapper(typeof(NoteProfile).Assembly);
+
 
             return services;
         }
